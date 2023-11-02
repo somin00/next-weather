c6944d998f4fbddd839fcab6aabcc74785e94f75
somin <smhs12@naver.com>
2023-11-02 18:21:07 +0900

    feat: 페이지 이동 설정

--- src/app/[location]/page.tsx
+++ src/app/[location]/page.tsx
@@ -1,10 +1,16 @@
+import HomeButton from "@/components/HomeButton";
+
 type Props = {
   params: { location: string };
 };
 export default function Detail({ params }: Props) {
+  const location = params.location;
+  const name = location === "seoul" ? "서울" : location === "newyork" ? "뉴욕" : "런던";
+
   return (
     <>
-      <h1> {params.location} 일기 예보</h1>
+      <h1> {name} 일기 예보</h1>
+      <HomeButton />
     </>
   );
 }
